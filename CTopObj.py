864b4226d1ad673606e1489c85ea333d1d56597c
Xiaohua Zhang <zhang30@llnl.gov>
2017-12-19 16:39:58 -0800

    Incomplete charmmTop2Obj

---
+++ CTopObj.py
@@ -0,0 +1,11 @@
+__author__ = 'zhang30'
+
+import CharmmTop
+from Pdb import Coor
+
+class CTopObj:
+    def __init__(self, charmmTop):
+        self.charmmTop = charmmTop
+
+    def toObj(self, args):
+        return
---
+++ charmmTop2obj.py
@@ -0,0 +1,29 @@
+__author__ = 'zhang30'
+
+
+import argparse
+import CharmmTop
+import Pdb
+import Obj
+import Specie
+
+
+def getArgs():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--top', action='store', dest='topfile', default='top_all22_prot.inp', help='CHARMM topology file (default=top_all22_prot.inp).')
+    parser.add_argument('-o', '--obj', action='store', dest='objfile', default='atom#.data', help='ddcMD object output file (default=atom#.data).')
+
+    args = parser.parse_args()
+
+    return args
+
+if __name__ == '__main__':
+
+    args=getArgs()
+    print "Default inputs: ", args.topfile,  args.objfile
+
+    print "Reading in CHARMM topology file ", args.topfile
+    charmmTop=CharmmTop.CharmmTop()
+    charmmTop.parse(args.topfile)
+
