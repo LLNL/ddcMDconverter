25d775f9296253ee26914836dbef3f77cfbb72e3
Francesco Di Natale <dinatale3@llnl.gov>
2018-03-20 13:46:27 -0700

    Addition of entry points.

--- setup.py
+++ setup.py
@@ -9,6 +9,10 @@ setup(name='ddcmdconverter',
       packages=find_packages(),
       entry_points={
         'console_scripts': [
+            'martini2obj = ddcmdconverter.martini2obj:main',
+            'pdbmartini2obj = ddcmdconverter.pdbmartini2obj:main',
+            'pdb2obj = ddcmdconverter.pdb2obj:main',
+            'obj2pdb = ddcmdconverter.obj2pdb:main',
         ]
       },
       install_requires=[],
